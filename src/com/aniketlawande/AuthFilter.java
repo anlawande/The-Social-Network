368748679b1da0d0ad010fc3d3359bfb37fd5449
Aniket Lawande <anlawande@gmail.com>
2016-03-19 19:55:46 -0400

    Basic filter for auth

---
+++ src/com/aniketlawande/AuthFilter.java
@@ -0,0 +1,48 @@
+package com.aniketlawande;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest)req;
+		HttpServletResponse response = (HttpServletResponse)res;
+		HttpSession session = request.getSession();
+		
+		boolean isAuth = true;
+		if (session.getAttribute("userid") == null) {
+			if (!request.getRequestURI().contains("/public/"))
+				isAuth = false;
+		}
+		
+		if (isAuth)
+			chain.doFilter(req, res);
+		else
+			response.sendRedirect("/The-Social-Network/public/index.html");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
